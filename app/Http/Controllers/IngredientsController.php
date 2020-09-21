88eb3a274db93026fa169d8f23ab2a5944707cd9
Valery Bogomolov <texter.coretext@gmail.com>
2020-09-21 14:18:05 +0300

    refactor

--- app/Http/Controllers/IngredientsController.php
+++ app/Http/Controllers/IngredientsController.php
@@ -27,12 +27,12 @@ class IngredientsController extends Controller
 
     public function store()
     {
+        $currentUser = \auth()->user();
+
         $data = \request()->validate([
             'name' => 'required|unique:ingredients',
             'price' => 'required',
         ]);
-
-        $currentUser = \auth()->user();
 
         $currentUser->ingredients()->create($data);
         return \redirect('/profile/' . $currentUser->id);
@@ -47,9 +47,6 @@ class IngredientsController extends Controller
     public function edit(Ingredient $ingredient)
     {
         $currentUser = \auth()->user();
-        if (is_null($currentUser)) {
-            return \redirect('/login');
-        }
         return view('ingredients.edit', compact('ingredient'));
     }
 
