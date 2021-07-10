756ea3650e0b2407aade4a6b30f3deb47270364a
Text_r <CoreText@users.noreply.github.com>
2021-07-11 00:53:57 +0300

    vscode test remote repository

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\ {
     Route
 };
 
+use App\Http\Controllers\ {
+    IngredientsController,
+    HomeController,
+    PizzasController,
+    ProfilesController
+};
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,28 +23,28 @@ use Illuminate\Support\Facades\ {
 |
 */
 
-Route::get('/', [App\Http\Controllers\PizzasController::class, 'index']);
+Route::get('/', [PizzasController::class, 'index']);
 
 Auth::routes();
 
 // Pages
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('home', [HomeController::class, 'index'])->name('home');
 
 // Profiles
-Route::get('/profiles/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
+Route::get('profiles/{user}', [ProfilesController::class, 'index'])->name('profile.show');
 
 // Pizzas
-Route::post('/pizzas', [App\Http\Controllers\PizzasController::class, 'store']);
-Route::get('/pizzas/create', [App\Http\Controllers\PizzasController::class, 'create']);
-Route::get('/pizzas/{pizza}/edit', [App\Http\Controllers\PizzasController::class, 'edit'])->name('pizza.edit');
-Route::patch('/pizzas/{pizza}', [App\Http\Controllers\PizzasController::class, 'update'])->name('pizza.update');
-Route::get('/pizzas/{pizza}', [App\Http\Controllers\PizzasController::class, 'show']);
-Route::get('/pizzas/{pizza}/delete', [App\Http\Controllers\PizzasController::class, 'delete']);
+Route::post('pizzas', [PizzasController::class, 'store']);
+Route::get('pizzas/create', [PizzasController::class, 'create']);
+Route::get('pizzas/{pizza}/edit', [PizzasController::class, 'edit'])->name('pizza.edit');
+Route::patch('pizzas/{pizza}', [PizzasController::class, 'update'])->name('pizza.update');
+Route::get('pizzas/{pizza}', [PizzasController::class, 'show']);
+Route::get('pizzas/{pizza}/delete', [PizzasController::class, 'delete']);
 
 // Ingredients
-Route::post('/ingredients', [App\Http\Controllers\IngredientsController::class, 'store']);
-Route::get('/ingredients/create', [App\Http\Controllers\IngredientsController::class, 'create']);
-Route::get('/ingredients/{ingredient}/edit', [App\Http\Controllers\IngredientsController::class, 'edit'])->name('ingredient.edit');
-Route::put('/ingredients/{ingredient}', [App\Http\Controllers\IngredientsController::class, 'update'])->name('ingredient.update');
-Route::get('/ingredients/{ingredient}', [App\Http\Controllers\IngredientsController::class, 'show']);
-Route::get('/ingredients/{ingredient}/delete', [App\Http\Controllers\IngredientsController::class, 'delete']);
+Route::post('ingredients', [IngredientsController::class, 'store']);
+Route::get('ingredients/create', [IngredientsController::class, 'create']);
+Route::get('ingredients/{ingredient}/edit', [IngredientsController::class, 'edit'])->name('ingredient.edit');
+Route::put('ingredients/{ingredient}', [IngredientsController::class, 'update'])->name('ingredient.update');
+Route::get('ingredients/{ingredient}', [IngredientsController::class, 'show']);
+Route::get('ingredients/{ingredient}/delete', [IngredientsController::class, 'delete']);
